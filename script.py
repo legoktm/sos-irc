c5e015435685bf4a7b5b17d5246f073a339882b4
Kunal Mehta <legoktm@gmail.com>
2014-12-27 22:19:04 -0800

    Initial commit

---
+++ script.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import json
+import requests
+import twitter
+import ur1
+
+with open('private.json', 'r') as f:
+    conf = json.load(f)
+
+ircnotifier_key = conf.pop('ircnotifier_key')
+api = twitter.Api(**conf)
+
+
+def build_url(_id):
+    return ur1.shorten('https://twitter.com/swiftonsecurity/status/%s' % _id)
+
+
+def send(msg):
+    print(msg)
+    requests.post('http://ircnotifier-test-01/v1/send', data={
+        'token': ircnotifier_key,
+        'channels': '#mediawiki-core',
+        'message': msg
+    })
+    pass
+
+last_id = None
+statuses = api.GetUserTimeline(screen_name='swiftonsecurity', exclude_replies=True, since_id=last_id)
+for status in statuses:
+    print(status.id)
+    if status.id > last_id:
+        last_id = status.id
+    send(
+        status.text.replace('\n', ' ').replace('\r', ' ')
+        + ' '.join(u.url for u in status.urls)
+        + ' - ' + build_url(status.id)
+    )
